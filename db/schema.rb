cfd23353f841187f8a5a9083e1b17c95179b6aa3
Risse Rigdon <risse.rigdon@gmail.com>
2016-04-02 02:01:18 +0000

    Add description to sections, make form for new categories

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160402012656) do
+ActiveRecord::Schema.define(version: 20160402015808) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20160402012656) do
     t.string   "name",        null: false
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "description"
   end
 
   add_index "sections", ["category_id"], name: "index_sections_on_category_id", using: :btree

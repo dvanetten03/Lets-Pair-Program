cbf9c4b95ec8e2f2a9c8f01ae099cc430a2458c9
Diane <diane@code12labs.com>
2016-04-04 21:37:47 +0000

    Add admin column to users table, add boolean argument to Admin users and setup factory girl user

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -28,6 +28,7 @@ ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   # Test helpers for Devise
+  config.include FactoryGirl::Syntax::Methods
   config.include Devise::TestHelpers, type: :controller
   config.include Devise::TestHelpers, type: :view
 

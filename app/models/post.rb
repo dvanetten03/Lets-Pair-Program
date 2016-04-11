694c0fb5a3583692efee865b28d4cd4cc9b2a3d0
risukun <risse.rigdon@gmail.com>
2016-04-11 15:35:14 +0000

    Add files for static_pages controller and posts

---
+++ app/models/post.rb
@@ -0,0 +1,6 @@
+class Post < ActiveRecord::Base
+  belongs_to :section
+  belongs_to :parent_post, :class_name => "Post", :foreign_key => "parent_post_id"
+  has_many :replies, :class_name => "Post", :foreign_key => "parent_post_id"
+
+end

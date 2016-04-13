1b841e874c284a3ea2f3d1ed4dbd8fc61819b799
Diane Van Etten <diane.vanetten@gmail.com>
2016-04-13 22:47:09 +0000

    Add wording for Terms and Condiditons Page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160405163341) do
+ActiveRecord::Schema.define(version: 20160405065017) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,8 +47,7 @@ ActiveRecord::Schema.define(version: 20160405163341) do
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
     t.boolean  "admin",                  default: false
-    t.string   "name",                                   null: false
-    t.string   "password_digest"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree

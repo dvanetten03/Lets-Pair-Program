95d8b118a4f37d47048c0cd5ddc4799e5291b8b8
risukun <risse.rigdon@gmail.com>
2016-04-14 18:24:35 +0000

    Add factory for categories, clean posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,3 @@
 class PostsController < ApplicationController
-	def index
-	end
-
-	def about
-	end
-
-	def contacts
-	end
-
-	def press
-	end
-
-	def terms
-	end
+	
 end
--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :category do
-    
+    name "News"
+    description "A place for news"
   end
 end

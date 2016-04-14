58cef105eb8bb863bec5175c4592a87b6476684f
risukun <risse.rigdon@gmail.com>
2016-04-14 21:00:24 +0000

    Move the user creation to inside the spec to get rid of an error while running rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  @user = FactoryGirl.create(:user)
 
   it "should not have duplicate email addresses" do
+      @user = FactoryGirl.create(:user)
       duplicate_user_email = @user.email.upcase
       @user.save
       assert_not duplicate_user_email.valid?
-    end
+  end
   
 end

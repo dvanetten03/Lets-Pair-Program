bcdeab32d210d161e52c5f9b9701fa0784f45f0e
Risse Rigdon <risse.rigdon@gmail.com>
2016-04-14 22:29:29 +0000

    Fix duplicate email test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,10 +3,12 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 
   it "should not have duplicate email addresses" do
-      @user = FactoryGirl.create(:user)
-      duplicate_user_email = @user.email.upcase
-      @user.save
-      assert_not duplicate_user_email.valid?
+    @user = FactoryGirl.create(:user)
+    duplicate_user_email = @user.email.upcase # FAKENAME13@gmail.com
+    @duplicateUser = FactoryGirl.build(:user, email: duplicate_user_email)
+    @duplicateUser.save
+    # there's probably a better way to write this line below.
+    expect(@duplicateUser.invalid?).to eq(true)
   end
   
 end

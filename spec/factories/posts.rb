282797ebf4a776781e0971ffba258aeb7becde54
risukun <risse.rigdon@gmail.com>
2016-04-14 18:42:17 +0000

    Add index page for posts

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,5 +1,5 @@
 FactoryGirl.define do
   factory :post do
-    
+   
   end
 end
--- spec/factories/sections.rb
+++ spec/factories/sections.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :section do
-    
+    name "Intros"
+    description "A place for intros"
   end
 end

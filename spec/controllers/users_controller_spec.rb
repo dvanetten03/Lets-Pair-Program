7f7836ff052a35865036da97158d36a96d317b44
Diane Van Etten <diane.vanetten@gmail.com>
2016-04-17 00:09:57 +0000

    Put users_controller text back

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,35 +3,19 @@ require 'rails_helper'
 RSpec.describe UsersController, type: :controller do
 
 
-  # describe "" do
+  describe "users#show action" do
 
-  #   it "should require users to be logged in to see their profile" do
-  #     get :new
-  #     expect(response).to redirect_to new_user_registration_path
-  #   end
+    it "should require users to be logged in to see their profile" do
+      get :show
+      expect(response).to redirect_to new_user_session_path
+    end
 
-  #   it "should successfully show profile page for correct user" do
-  #     user = FactoryGirl.create(:user)
-  #     sign_in user
+    it "should successfully show profile page for correct user" do
+      user = FactoryGirl.create(:user)
+      sign_in user
 
-  #     get :show, id: user.id
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
-
-  # # describe "users#new action" do
-
-  # #   it "should successfully create a new user in our database"
-
-  # describe "users#edit action" do
-
-  #   it "should successfully redirect to edit_user_registration_path" do
-  #     user = FactoryGirl.create(:user)
-  #     sign_in user
-
-  #     #get 
-
-  #   end
-  # end
-
+      get :show, id: user.id
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
